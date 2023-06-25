f771acaee33c771231e202a11e537525f88e3586
mlukicdesign <mlukicdesign@gmail.com>
2023-06-25 21:40:34 +0800

    codebase is cooked

--- Develop/controllers/api/index.js
+++ Develop/controllers/api/index.js
@@ -1,10 +1,8 @@
 const router = require('express').Router();
 const userRoutes = require('./userRoutes');
-const projectRoutes = require('./projectRoutes');
 const postRoute = require('./postRoute');
 
 router.use('/users', userRoutes);
-router.use('/projects', projectRoutes);
 router.use('/posts', postRoute);
 
 module.exports = router;
